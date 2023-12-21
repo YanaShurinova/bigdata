0247d4106418356c125cc0f45d078dfb7363d5c2
YanaShurinova <108683692+YanaShurinova@users.noreply.github.com>
2023-12-21 15:01:49 +0400

    Add files via upload

---
+++ lab4/src/main/scala/twoPhazeCommit/Coordinator.scala
@@ -0,0 +1,61 @@
+package twoPhazeCommit
+
+
+import org.apache.zookeeper.Watcher.Event.EventType
+import org.apache.zookeeper._
+import org.apache.zookeeper.data.Stat
+
+
+case class Coordinator(hostPort: String, root: String, clientCount: Int) extends Watcher {
+
+  val zk = new ZooKeeper(hostPort, 3000, this)
+  val mutex = new Object()
+  var decision: String = "COMMIT"
+
+  if (zk == null) throw new Exception("ZK is NULL.")
+
+  // Реакция на события от Zookeeper
+  override def process(event: WatchedEvent): Unit = {
+    // Блок синхронизации
+    mutex.synchronized {
+      println(s"Event from keeper: ${event.getType}")
+      if (event.getType == EventType.NodeChildrenChanged) {
+        mutex.notify()
+      }
+    }
+
+  }
+
+  def execute(): Unit = {
+
+    println("Waiting for clients")
+    mutex.synchronized {
+      var clients = zk.getChildren(root, this)
+      while (clients.size() < clientCount) {
+        mutex.wait()
+        println("Client connected")
+        clients = zk.getChildren(root, this)
+      }
+    }
+
+    println("Reading clients")
+    val clients = zk.getChildren(root, true)
+
+    clients.forEach(client => {
+      val clientDecision = new String(zk.getData(root + "/" + client, false, new Stat()))
+      println("Client " + client + " decided to " + clientDecision)
+      if (clientDecision.equals("ROLLBACK") || decision.equals("ROLLBACK")) {
+        decision = "ROLLBACK"
+      }
+    })
+
+    println("Asking clients to " + decision)
+
+    clients.forEach(client => {
+      zk.setData(root +"/"+ client, decision.getBytes(), 0)
+    })
+    println("Finished coordination")
+
+  }
+
+}
